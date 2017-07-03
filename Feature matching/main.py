bba11dfaa783bd3c261a878b0caae328800db87f
suwonLee <lsw7169@gmail.com>
2017-07-03 19:20:35 +0900

    matching 부분 추가

--- Feature matching/main.py
+++ Feature matching/main.py
@@ -6,14 +6,14 @@ Created on Mon Jul 03 00:36:02 2017
 """
 
 import numpy as np
-import cv2
+import cv2 as cv2
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('Kaleidoscope.jpg',0)
-img2 = cv2.imread('patch.jpg',0)
+img1 = cv2.imread('Kaleidoscope.jpg')
+img2 = cv2.imread('patch.jpg')
 
 # Initiate SIFT detector
-orb = cv2.ORB()
+orb = cv2.ORB_create()
 
 # find the keypoints and descriptors with SIFT
 kp1, des1 = orb.detectAndCompute(img1,None)
@@ -28,7 +28,11 @@ matches = bf.match(des1,des2)
 # Sort them in the order of their distance.
 matches = sorted(matches, key=lambda x:x.distance)
 
+# Convert BGR 2 RGB
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+
 # Draw first 10 matches
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10], flags=2)
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:100],None, flags=2)
 
 plt.imshow(img3),plt.show()
